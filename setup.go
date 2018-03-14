98900f578994a073cbfd052e752f421a8e767491
Amrit <me@amrit.be>
2018-03-13 18:36:37 -0700

    tabs to spaces, add xfer hook

--- setup.go
+++ setup.go
@@ -1,136 +1,136 @@
 package redis
 
 import (
-	"strconv"
+    "strconv"
 
-	"github.com/coredns/coredns/core/dnsserver"
-	"github.com/coredns/coredns/plugin"
-	"github.com/mholt/caddy"
-	"log"
-	"time"
+    "github.com/coredns/coredns/core/dnsserver"
+    "github.com/coredns/coredns/plugin"
+    "github.com/mholt/caddy"
+    "log"
+    "time"
 )
 
 func init() {
-	caddy.RegisterPlugin("redis", caddy.Plugin{
-		ServerType: "dns",
-		Action:     setup,
-	})
+    caddy.RegisterPlugin("redis", caddy.Plugin{
+        ServerType: "dns",
+        Action:     setup,
+    })
 }
 
 func setup(c *caddy.Controller) error {
-	r, err := redisParse(c)
-	if err != nil {
-		return plugin.Error("redis", err)
-	}
+    r, err := redisParse(c)
+    if err != nil {
+        return plugin.Error("redis", err)
+    }
 
-	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		r.Next = next
-		return r
-	})
+    dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+        r.Next = next
+        return r
+    })
 
-	return nil
+    return nil
 }
 
 func redisParse(c *caddy.Controller) (*Redis, error) {
-	redis := Redis{
-		keyPrefix: "",
-		keySuffix: "",
-		Ttl:       300,
-	}
-	var (
-		err error
-	)
+    redis := Redis{
+        keyPrefix: "",
+        keySuffix: "",
+        Ttl:       300,
+    }
+    var (
+        err error
+    )
 
-	for c.Next() {
-		if c.NextBlock() {
-			for {
-				switch c.Val() {
-				case "address":
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					redis.redisAddress = c.Val()
-				case "password":
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					redis.redisPassword = c.Val()
-				case "prefix":
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					redis.keyPrefix = c.Val()
-				case "suffix":
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					redis.keySuffix = c.Val()
-				case "connect_timeout":
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					redis.connectTimeout, err = strconv.Atoi(c.Val())
-					if err != nil {
-						redis.connectTimeout = 0
-					}
-				case "read_timeout":
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					redis.readTimeout, err = strconv.Atoi(c.Val())
-					if err != nil {
-						redis.readTimeout = 0
-					}
-				case "ttl":
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					var val int
-					val, err = strconv.Atoi(c.Val())
-					if err != nil {
-						val = TTL
-					}
-					redis.Ttl = uint32(val)
-				case "refresh_time":
-					// parse time in seconds
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					var val int
-					val, err = strconv.Atoi(c.Val())
-					if err != nil {
-						val = TTL
-					}
-					ZoneUpdateTime = time.Duration(val) * time.Second
-				case "database_index":
-					//set the default index
-					// parse time in seconds
-					if !c.NextArg() {
-						return &Redis{}, c.ArgErr()
-					}
-					var val int
-					val, err = strconv.Atoi(c.Val())
-					if err != nil {
-						val = 0
-					}
-					redis.redisDbIndex = val
-				default:
-					if c.Val() != "}" {
-						log.Printf("Warning: unknown property '%s'", c.Val())
-					}
-				}
+    for c.Next() {
+        if c.NextBlock() {
+            for {
+                switch c.Val() {
+                case "address":
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    redis.redisAddress = c.Val()
+                case "password":
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    redis.redisPassword = c.Val()
+                case "prefix":
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    redis.keyPrefix = c.Val()
+                case "suffix":
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    redis.keySuffix = c.Val()
+                case "connect_timeout":
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    redis.connectTimeout, err = strconv.Atoi(c.Val())
+                    if err != nil {
+                        redis.connectTimeout = 0
+                    }
+                case "read_timeout":
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    redis.readTimeout, err = strconv.Atoi(c.Val())
+                    if err != nil {
+                        redis.readTimeout = 0
+                    }
+                case "ttl":
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    var val int
+                    val, err = strconv.Atoi(c.Val())
+                    if err != nil {
+                        val = TTL
+                    }
+                    redis.Ttl = uint32(val)
+                case "refresh_time":
+                    // parse time in seconds
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    var val int
+                    val, err = strconv.Atoi(c.Val())
+                    if err != nil {
+                        val = TTL
+                    }
+                    ZoneUpdateTime = time.Duration(val) * time.Second
+                case "database_index":
+                    //set the default index
+                    // parse time in seconds
+                    if !c.NextArg() {
+                        return &Redis{}, c.ArgErr()
+                    }
+                    var val int
+                    val, err = strconv.Atoi(c.Val())
+                    if err != nil {
+                        val = 0
+                    }
+                    redis.redisDbIndex = val
+                default:
+                    if c.Val() != "}" {
+                        log.Printf("Warning: unknown property '%s'", c.Val())
+                    }
+                }
 
-				if !c.Next() {
-					break
-				}
-			}
+                if !c.Next() {
+                    break
+                }
+            }
 
-		}
+        }
 
-		redis.connect()
-		redis.LoadZones()
+        redis.connect()
+        redis.LoadZones()
 
-		return &redis, nil
-	}
-	return &Redis{}, nil
+        return &redis, nil
+    }
+    return &Redis{}, nil
 }
